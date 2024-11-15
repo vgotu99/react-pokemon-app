501b8666cc7d5eb089174d5f0e402a9cba641fcc
vgotu99 <vgotu99@gmail.com>
2024-11-15 21:11:04 +0900

    feat: create PokeCard component to display pokemon details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import PokeCard from "./components/PokeCard";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
@@ -13,7 +14,6 @@ function App() {
     try {
       const res = await axios.get(url);
       setPokemons(res.data.results);
-      console.log(res.data.results);
     } catch (error) {
       console.error(error);
     }
@@ -25,11 +25,11 @@ function App() {
         <header className="flex flex-col gap-2 w-full px-4 z-50">
           input form
         </header>
-        <section className="pt-6 flex flex-col items-center overflow-auto z-0">
+        <section className="pt-6 flex flex-col items-center justify-center overflow-auto z-0">
           <div className="flex flex-row flex-wrap gap-4 items-center justify-center px-2 max-w-4xl">
             {pokemons.length > 0 ? (
               pokemons.map(({ url, name }, index) => (
-                <div key={index}>{name}</div>
+                  <PokeCard key={url} url={url} name={name} />
               ))
             ) : (
               <h2 className="font-medium text-lg text-slate-900 mb-1">
---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+
+export const PokeCard = ({ url, name }) => {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetchPokeDetailData();
+  }, []);
+
+  const fetchPokeDetailData = async () => {
+    try {
+      const res = await axios.get(url);
+      const pokemonData = formatPokemonData(res.data);
+      setPokemon(pokemonData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const formatPokemonData = (params) => {
+    const { id, types, name } = params;
+    const pokeData = {
+      id,
+      name,
+      type: types[0].type.name,
+    };
+    return pokeData;
+  };
+
+  const bg = `bg-${pokemon?.type}`;
+  const border = `border-${pokemon?.type}`;
+  const text = `text-${pokemon?.type}`;
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+
+  return (
+    <>
+    
+      {pokemon && (
+        <a
+          href={`/pokemon/${name}`}
+          className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0  bg-slate-800`}
+        >
+          <div className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`} >
+            #{pokemon.id.toString().padStart(3, '0')} {/* padStart(글자수, '채울 값') */}
+          </div>
+          <div className={`w-full flex items-center justify-center`}>
+            <div className={`box-border relative flex w-full h-[5.5rem] basis-auto justify-center items-center`} >
+              <img src={img} alt={name} width='100%' className={`object-contain h-full`} />
+            </div>
+          </div>
+          <div className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1 text-center`}>
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </>
+  );
+};
+
+export default PokeCard;
