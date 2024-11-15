69a186e1844404d0b279d2207a36a32c1ee1c73e
vgotu99 <vgotu99@gmail.com>
2024-11-16 04:51:12 +0900

    feat: add autocomplete functionality for search term

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import PokeCard from "./components/PokeCard";
 import { useDebounce } from "./hooks/useDebounce";
+import AutoComplete from "./components/AutoComplete";
 
 function App() {
   const [allPokemons, setAllPokemons] = useState([]);
   const [displayedPokemons, setDisplayedPokemons] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   const limitNum = 20;
   const url = `https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0`;
@@ -15,10 +14,6 @@ function App() {
   useEffect(() => {
     fetchPokeData();
   }, []);
-
-  useEffect(() => {
-    handleSearchInput(debouncedSearchTerm);
-  }, [debouncedSearchTerm]);
 
   const filterDisplayedPokemonData = (
     allPokemonsData,
@@ -41,46 +36,11 @@ function App() {
     }
   };
 
-  const handleSearchInput = async (debouncedSearchTerm) => {
-    if (searchTerm.length > 0) {
-      try {
-        const res = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${debouncedSearchTerm}`
-        );
-        const pokemonData = {
-          url: `https://pokeapi.co/api/v2/pokemon/${res.data.id}`,
-          name: debouncedSearchTerm,
-        };
-        setDisplayedPokemons([pokemonData]);
-      } catch (error) {
-        setDisplayedPokemons([]);
-        console.error(error);
-      }
-    } else {
-      fetchPokeData(true);
-    }
-  };
-
   return (
     <>
       <article className="pt-6">
         <header className="flex flex-col gap-2 w-full px-4 z-50">
-          <div className="relative z-50">
-            <form className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
-              <input
-                type="text"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="text-xs w-[20.5rem] h-6 px-2 py-1 rounded-lg text-gray-300 text-center bg-[hsl(214,13%,47%)]"
-              />
-              <button
-                type="submit"
-                className="text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700"
-              >
-                검색
-              </button>
-            </form>
-          </div>
+          <AutoComplete allPokemons={allPokemons} setDisplayedPokemons={setDisplayedPokemons} />
         </header>
         <section className="pt-6 flex flex-col items-center justify-center overflow-auto z-0">
           <div className="flex flex-row flex-wrap gap-4 items-center justify-center px-2 max-w-4xl">
