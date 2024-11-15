135b2ad13b874645b55ec8cbfadb532c5c4b1e77
vgotu99 <vgotu99@gmail.com>
2024-11-15 21:41:43 +0900

    feat: implement pokemon search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,61 @@ import PokeCard from "./components/PokeCard";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const url = "https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0";
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    fetchPokeData();
+    fetchPokeData(true);
   }, []);
 
-  const fetchPokeData = async () => {
+  const fetchPokeData = async (isFirstFetch) => {
     try {
+      const offsetValue = isFirstFetch ? 0 : offset + limit
+      const url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offsetValue}`;
       const res = await axios.get(url);
-      setPokemons(res.data.results);
+      setPokemons([...pokemons, ...res.data.results]);
+      setOffset(offsetValue)
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleSearchInput = async e => {
+    setSearchTerm(e.target.value)
+    if(e.target.value.length > 0) {
+      try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${e.target.value}`)
+        const pokemonData = {
+          url:`https://pokeapi.co/api/v2/pokemon/${res.data.id}`,
+          name: searchTerm
+        }
+        setPokemons([pokemonData])
+      } catch (error) {
+        setPokemons([])
+        console.error(error)
+      }
+    } else {
+      fetchPokeData(true)
+    }
+  }
+
   return (
     <>
       <article className="pt-6">
         <header className="flex flex-col gap-2 w-full px-4 z-50">
-          input form
+          <div className="relative z-50">
+            <form className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
+              <input type="text" value={searchTerm} onChange={handleSearchInput} className="text-xs w-[20.5rem] h-6 px-2 py-1 rounded-lg text-gray-300 text-center bg-[hsl(214,13%,47%)]" />
+              <button type="submit" className="text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700">검색</button>
+            </form>
+          </div>
         </header>
         <section className="pt-6 flex flex-col items-center justify-center overflow-auto z-0">
           <div className="flex flex-row flex-wrap gap-4 items-center justify-center px-2 max-w-4xl">
             {pokemons.length > 0 ? (
               pokemons.map(({ url, name }, index) => (
-                  <PokeCard key={url} url={url} name={name} />
+                <PokeCard key={url} url={url} name={name} />
               ))
             ) : (
               <h2 className="font-medium text-lg text-slate-900 mb-1">
@@ -38,6 +67,14 @@ function App() {
             )}
           </div>
         </section>
+        <div className="text-center">
+          <button
+            onClick={() => fetchPokeData(false)}
+            className="bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white"
+          >
+            더 보기
+          </button>
+        </div>
       </article>
     </>
   );
