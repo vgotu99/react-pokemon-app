6702149cb9dc24b3efb871a5e6d62047ac7900e0
vgotu99 <vgotu99@gmail.com>
2024-11-15 19:53:55 +0900

    feat: fetch pokemon data from pokeapi and display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0";
+
+  useEffect(() => {
+    fetchPokeData();
+  }, []);
+
+  const fetchPokeData = async () => {
+    try {
+      const res = await axios.get(url);
+      setPokemons(res.data.results);
+      console.log(res.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>
-        Hello Tailwind
-      </h1>
-    </div>
-  )
+    <>
+      <article className="pt-6">
+        <header className="flex flex-col gap-2 w-full px-4 z-50">
+          input form
+        </header>
+        <section className="pt-6 flex flex-col items-center overflow-auto z-0">
+          <div className="flex flex-row flex-wrap gap-4 items-center justify-center px-2 max-w-4xl">
+            {pokemons.length > 0 ? (
+              pokemons.map(({ url, name }, index) => (
+                <div key={index}>{name}</div>
+              ))
+            ) : (
+              <h2 className="font-medium text-lg text-slate-900 mb-1">
+                포켓몬이 없습니다.
+              </h2>
+            )}
+          </div>
+        </section>
+      </article>
+    </>
+  );
 }
 
-export default App
+export default App;
